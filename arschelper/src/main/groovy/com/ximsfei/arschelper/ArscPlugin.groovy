91d386d8f943a701864b7ceeb4ff1cf908cc252e
pengfengwang <pengfengwang@sohu-inc.com>
2017-08-08 10:31:26 +0800

    init arschelper

---
+++ arschelper/src/main/groovy/com/ximsfei/arschelper/ArscPlugin.groovy
@@ -0,0 +1,11 @@
+package com.ximsfei.arschelper
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ArscPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
